928d3377de24f0aa16749abf456a59f2211d82fa
zaidkamil <xaidmetamorphos@gmail.com>
2022-08-13 14:43:41 +0530

    pygame_opencv

--- data_science/hand_detection_2.py
+++ data_science/hand_detection_2.py
@@ -1,54 +1,65 @@
 import cv2
 import mediapipe as mp
+import pgzrun
+import sys
+WIDTH = 640
+HEIGHT = 350
+
+
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 mp_hands = mp.solutions.hands
 
-fourcc = cv2.VideoWriter_fourcc(*'XVID')
-out = cv2.VideoWriter('hand_detector.avi', fourcc, 20.0, (640,  480))
 
 cap = cv2.VideoCapture(0)
-with mp_hands.Hands(
-    model_complexity=0,
-    min_detection_confidence=0.5,
-    min_tracking_confidence=0.5) as hands:
-  while cap.isOpened():
-    success, image = cap.read()
-    if not success:
-      print("Ignoring empty camera frame.")
-      # If loading a video, use 'break' instead of 'continue'.
-      continue
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 
-    # To improve performance, optionally mark the image as not writeable to
-    # pass by reference.
-    image.flags.writeable = False
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    results = hands.process(image)
+box = Rect((0, 250,), (10,10))
+hands = mp_hands.Hands(model_complexity=0, min_detection_confidence=0.5, min_tracking_confidence=0.5)
 
-    # Draw the hand annotations on the image.
-    image.flags.writeable = True
-    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+def draw():
+    # screen.fill('white')
+    screen.draw.filled_rect(box, 'red')
 
+def update():
+
+    if cap.isOpened():
+        success, image = cap.read()
+        if not success:
+            print("Ignoring empty camera frame.")
+        image.flags.writeable = False
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        results = hands.process(image)
+        image.flags.writeable = True
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+        width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+        height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+
+        if results.multi_hand_landmarks:
+            for hand_landmarks in results.multi_hand_landmarks:
+                mp_drawing.draw_landmarks(
+                    image,
+                    hand_landmarks,
+                    mp_hands.HAND_CONNECTIONS,
+                    mp_drawing_styles.get_default_hand_landmarks_style(),
+                    mp_drawing_styles.get_default_hand_connections_style())
+                data = hand_landmarks.landmark[8]
+                coords = mp_drawing._normalized_to_pixel_coordinates(data.x, data.y, width, height)
+                cv2.circle(image, coords, 10, (0, 255, 255), -1)
+                print(coords)
+                try:
+                    box.center = (WIDTH-coords[0], coords[1])
+                except Exception as e:
+                    print(e)
+        cv2.flip(image, 1)
+        cv2.line(image, (0,350), (640,350), (255, 255, 255), 2)
+        cv2.putText(image, "OPENCV + PYGAME + MEDIAPIPE", (640//3, 400), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 255), 2)
+        cv2.imshow('MediaPipe Hands', image)
+        if cv2.waitKey(5) & 0xFF == 27:
+            cap.release()
+            cv2.destroyAllWindows()
+            sys.exit()
+
+pgzrun.go()
     
-
-    width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
-    height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
-
-    if results.multi_hand_landmarks:
-      for hand_landmarks in results.multi_hand_landmarks:
-        mp_drawing.draw_landmarks(
-            image,
-            hand_landmarks,
-            mp_hands.HAND_CONNECTIONS,
-            mp_drawing_styles.get_default_hand_landmarks_style(),
-            mp_drawing_styles.get_default_hand_connections_style())
-        data = hand_landmarks.landmark[8]
-        coords = mp_drawing._normalized_to_pixel_coordinates(data.x, data.y, width, height)
-        cv2.circle(image, coords, 50, (0, 0, 255), -1)
-    cv2.imshow('MediaPipe Hands', cv2.flip(image, 1))
-    out.write(image)
-    if cv2.waitKey(5) & 0xFF == 27:
-      break
-cap.release()
-out.release()
-cv2.destroyAllWindows()
